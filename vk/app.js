83fc480118529efdcaec65320a13d3ac39dadfee
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-24 18:51:27 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -17,7 +17,6 @@ app.controller("AppCtrl", function ($scope) {
     $scope.authInfo = function(response){
         if(response.session){ // Авторизация успешна
             show();
-            show();
             function show() {
                 $scope.vk.data.user = response.session.user;
                 $(".current_user").text($scope.vk.data.user.first_name);
