6649b13c0de233fa78df14f2628b678d55a255ff
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-25 18:52:05 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -43,6 +43,7 @@ app.controller("AppCtrl", function ($scope) {
                 for (var i = 0; i<$scope.vk.albums.length; i++) {
                     VK.Api.call('photos.get', {owner_id: $scope.vk.data.user.id, album_id: $scope.vk.albums[i].id, rev: 1, extended: 1, count: 1000}, function (r) {
                         if (r.response) {
+                            alert(1)
                             var obj = {album_id: $scope.vk.albums[j].id, album_title: $scope.vk.albums[j].title, album_content: r.response};
                                 $scope.vk.albums_content[$scope.vk.albums_content.length] = obj
                             j++;
