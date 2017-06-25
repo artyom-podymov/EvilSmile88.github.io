223d5674fd400e3f93923305a06bf18edb3131d9
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-25 21:02:32 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -47,12 +47,12 @@ app.controller("AppCtrl", function ($scope) {
                                 $scope.vk.albums_content[$scope.vk.albums_content.length] = obj;
                         }
                     })
-                    (function (id) {
+                    setTimeout(function (id) {
                                 return function() {
                                     $scope.vk.albums_content.album_id = id;
                                     alert(id)
                                 }
-                            })(id)
+                            },1000)
                 }
 //                 for (var i=0; i<$scope.vk.albums; i++) {
 //                     for (var j = 0; j < $scope.vk.albums_content.length; j++) {
