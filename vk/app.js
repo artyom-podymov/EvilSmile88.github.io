53469dbbf4123ae8d3aa85daa7fdd32c0ab4d7f6
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-25 19:44:51 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -43,20 +43,18 @@ app.controller("AppCtrl", function ($scope) {
                     VK.Api.call('photos.get', {owner_id: $scope.vk.data.user.id, album_id: $scope.vk.albums[i].id, rev: 1, extended: 1, count: 1000}, function (r) {
                         if (r.response) {
                             var obj = {album_content: r.response};
-                                if (obj.album_content.lenght > 0) {
                                 obj.album_id: r.response[0].aid;
                                 $scope.vk.albums_content[$scope.vk.albums_content.length] = obj;
-                                    }
                         }
                     })
                 }
-                for (var i=0; i<$scope.vk.albums; i++) {
-                    for (var j = 0; j < $scope.vk.albums_content.length; j++) {
-                    if ($scope.vk.albums[i].id == $scope.vk.albums_content[j].album_id) {
-                        $scope.vk.albums_content[j].album_title = $scope.vk.albums[i].title;
-                        }
-                    }
-                }
+//                 for (var i=0; i<$scope.vk.albums; i++) {
+//                     for (var j = 0; j < $scope.vk.albums_content.length; j++) {
+//                     if ($scope.vk.albums[i].id == $scope.vk.albums_content[j].album_id) {
+//                         $scope.vk.albums_content[j].album_title = $scope.vk.albums[i].title;
+//                         }
+//                     }
+//                 }
                 console.log($scope.vk.albums_content)
         }, 1000);
 
