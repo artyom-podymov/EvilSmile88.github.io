7ad6076df84344438e96f4547ed97fb67c61eb05
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-25 18:21:09 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -11,7 +11,6 @@ app.controller("AppCtrl", function ($scope) {
         appID: 6085608,
         appPermissions: 7,
         albums_content: [],
-        albums: [{id: -6, title: "Profile"}, {id:-7, title: "Wall"}],
         init: function(){
             VK.Auth.login($scope.authInfo, $scope.vk.appPermissions);
         }
@@ -29,16 +28,18 @@ app.controller("AppCtrl", function ($scope) {
             }
         }else alert("Авторизоваться не удалось!");
         VK.Api.call('photos.getAlbums', {owner_id: $scope.vk.data.user.id}, function (r) {
+            var albums =[{id: -6, title: "Profile"}, {id:-7, title: "Wall"}];
             if (r.response) {
                 for (var i=0; i<r.response.length; i++) {
                     var obj = {id: r.response[i].aid, title: r.response[i].title};
-                    $scope.vk.albums[$scope.vk.albums.length] = obj;
+                    albums[albums.length] = obj;
                 }
+                $scope.vk.albums = albums;
                 console.log($scope.vk.albums);
             }
         })
-        setTimeout(function () {
-            var j=0;
+        var intervalHandler = setInterval(function () {
+            if ($scope.vk.albums) {
         for (var i = 0; i<$scope.vk.albums.length; i++) {
             VK.Api.call('photos.get', {owner_id: $scope.vk.data.user.id, album_id: $scope.vk.albums[i].id, rev: 1, extended: 1, count: 1000}, function (r) {
                 if (r.response) {
@@ -51,6 +52,8 @@ app.controller("AppCtrl", function ($scope) {
             })
         }
         console.log($scope.vk.albums_content)
+            clearInterval(intervalGandler);
+            }
         }, 100);
         
     }
