d78d3b918c0dfa9ab1814e60576184b797cffbe7
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-26 01:25:27 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -17,15 +17,16 @@ app.controller("AppCtrl", function ($scope) {
     };
     $scope.authInfo = function(response){
         if(response.session){ // Авторизация успешна
-            
-            (function() {
+            show();
+            show();
+            function show() {
                 $scope.vk.data.user = response.session.user;
 //                 $(".current_user").text($scope.vk.data.user.first_name);
                 console.log($scope.vk.data.user)
                 $(".current_file").slideDown("slow", function () {
                     $(".current_file").css("display","flex")
                 })
-            })()
+            }
         }else alert("Авторизоваться не удалось!");
         VK.Api.call('photos.getAlbums', {owner_id: $scope.vk.data.user.id}, function (r) {
             var albums =[{id: -6, title: "Profile"}, {id:-7, title: "Wall"}];
