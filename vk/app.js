d0c5b18a094ceaae32290d51fffca3047e2d741e
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-25 16:16:21 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -7,13 +7,15 @@ app.controller("AppCtrl", function ($scope) {
         {class: "current_nav", url: "img/gallery.png", title: "gallery", alt: "gallery"}
     ]
     $scope.vk = {
-            data: {},
-            appID: 6085608,
-            appPermissions: 7,
-            init: function(){
-                VK.Auth.login($scope.authInfo, $scope.vk.appPermissions);
-            }
-        };
+        data: {},
+        appID: 6085608,
+        appPermissions: 7,
+        albums: [],
+        albums_id: [-6, -7],
+        init: function(){
+            VK.Auth.login($scope.authInfo, $scope.vk.appPermissions);
+        }
+    };
     $scope.authInfo = function(response){
         if(response.session){ // Авторизация успешна
             show();
@@ -26,9 +28,16 @@ app.controller("AppCtrl", function ($scope) {
                 })
             }
         }else alert("Авторизоваться не удалось!");
-        VK.Api.call('photos.get', {owner_id: $scope.vk.data.user.id, album_id: "wall", count: 10}, function (r) {
+        VK.Api.call('photos.getAlbums', {owner_id: $scope.vk.data.user.id}, function (r) {
+          if (r.response) {
+              for (var i=0; i<r.response.length-1; i++) {
+                  $scope.vk.albums_id[$scope.vk.albums_id.length] = r.response[i].id
+              }
+              console.log($scope.vk.albums_id)
+          }
+        })
+        VK.Api.call('photos.get', {owner_id: $scope.vk.data.user.id, album_id: "wall", rev: 1, extended: 1, count: 1000}, function (r) {
             if (r.response) {
-                alert("Фото");
                 $scope.vk.data.wall_photos = r.response;
                 console.log($scope.vk.data.wall_photos)
             }
@@ -40,3 +49,4 @@ app.controller("AppCtrl", function ($scope) {
         event.currentTarget.setAttribute("class", "current_nav")
     }
 })
+
