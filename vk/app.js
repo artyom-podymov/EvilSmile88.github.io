aae8075b7caaf1cdd3a65deb59b2ad001ccef06a
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-26 04:02:04 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -42,6 +42,7 @@ app.controller("AppCtrl", function ($scope) {
             }
         })
             
+           setTimeout( function () {
             for (var i = 0; i<$scope.vk.albums.length; i++) {
                 VK.Api.call('photos.get', {owner_id: $scope.vk.data.user.id, album_id: $scope.vk.albums[i].id, rev: 1, extended: 1, count: 1000}, function (r) {
                     if (r.response) {
@@ -49,7 +50,8 @@ app.controller("AppCtrl", function ($scope) {
                         $scope.vk.albums_content[$scope.vk.albums_content.length] = obj;
                     }
                 })
-            }    
+            }
+        },1000)
         
 //                 (function () {
 //                 for (var i=0; i<$scope.vk.albums.length; i++) {
@@ -61,9 +63,7 @@ app.controller("AppCtrl", function ($scope) {
 //                 }
 //                 })()
                 console.log($scope.vk.albums_content)
-   
-
-    }
+       }
     $scope.currentNav = function (event) {
         $(".nav div").removeClass("current_nav").addClass("not_current_nav")
         event.currentTarget.setAttribute("class", "current_nav")
