7fa900ef56394ac2301d0bdf67625825a8c79d5f
EvilSmile88 <evilsmiles88@gmail.com>
2017-06-27 20:57:38 +0300

    Update app.js

--- vk/app.js
+++ vk/app.js
@@ -74,7 +74,7 @@ app.controller("AppCtrl", function ($scope) {
     }
     $scope.currentAlbum = function (event) {
         $scope.place = "Photos";
-        alert(this.id)
+        alert(event.currentTarget.getAttribute("id")
     }
 })
 
